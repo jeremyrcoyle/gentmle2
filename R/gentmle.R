14bb60a9e3303c2c301040253c3c117ecb425c44
Jonathan Levy <jl@jlstiles.com>
2017-06-03 15:07:38 -0700

    restoring Jeremy's latest version--I think I may have screwed something up today...

--- R/gentmle.R
+++ R/gentmle.R
@@ -1,7 +1,10 @@
 library(boot)
-############## somewhat general TMLE framework takes initial data, and a tmle parameter spec todo:
-############## reimplement truncation (can just be a separate submodel) implement gradient for
-############## optimization approaches handle parameters which require fluctuating g
+############## somewhat general TMLE framework takes initial data, and a tmle parameter spec todo: reimplement truncation (can just be a
+############## separate submodel) implement gradient for optimization approaches handle parameters which require fluctuating g
+
+# combined Q+g loss individual losses?  default ICs support different depsilons support different submodels/loss fns
+
+# IC as gradient MSMs multiple A values
 
 #' @title gentmle
 #' @description General TMLE function that takes care of the bookkeeping of estimation and update steps.
@@ -9,29 +12,29 @@ library(boot)
 #' @param paramlist, named list of parameters to estimate. See define_param for details
 #' @param submodel, submodel along which to fluctuate
 #' @param loss, loss function to optimize
+#' @param depsilon, small epsilon for the recurisve approach
 #' @param approach, One of initial, recursive (small delta), line, full
 #' @param max_iter, Maximum number of iteration steps
 #'
 #' @export
 ##' @example /inst/examples/ey1_example.R
 ##' @example /inst/examples/eysi_example.R
-gentmle <- function(initdata, params, submodel = submodel_logit, loss = loss_loglik,
-                    approach = "recursive", max_iter = 100, ...) {
+gentmle <- function(initdata, params, submodel = submodel_logit, loss = loss_loglik, depsilon=1e-4, approach = "full", max_iter = 100,
+                    ...) {
   tmleenv <- list2env(initdata)
   converge <- F
   last_risk <- Inf
 
   initests <- NULL
-  n <- length(tmledata$Y)
+  n <- length(initdata$Y)
   for (j in seq_len(max_iter)) {
-    # print('Qks') print(mean(tmleenv$Qk)) print(mean(tmleenv$Q1k))
-    # print(mean(tmleenv$Q0k))
+    # print('Qks') print(mean(tmleenv$Qk)) print(mean(tmleenv$Q1k)) print(mean(tmleenv$Q0k))
 
     # calculate parameters and associated values
     evals <- lapply(params, eval_param, tmleenv)
     Dstar <- sapply(evals, `[[`, "IC")
     ED <- apply(Dstar, 2, mean)
-    sig <- sapply(evals, function(param) sd(param$IC))
+    ED2 <- apply(Dstar^2, 2, mean)
     psi <- sapply(evals, `[[`, "psi")
     if (j == 1) {
       initests <- psi
@@ -40,8 +43,6 @@ gentmle <- function(initdata, params, submodel = submodel_logit, loss = loss_log
     HAs <- sapply(evals, `[[`, "HA")
     H1s <- sapply(evals, `[[`, "H1")
     H0s <- sapply(evals, `[[`, "H0")
-
-    # print('HAs') print(mean(HAs)) print(mean(H0s)) print(mean(H1s))
 
     if (approach == "full") {
       # optimize across multidimensional HA
@@ -60,45 +61,36 @@ gentmle <- function(initdata, params, submodel = submodel_logit, loss = loss_log
     if (approach == "initial") {
       eps <- 0
     } else if (approach == "recursive") {
-      eps <- 0.0001
+      eps <- depsilon
     } else if (approach == "line" || approach == "full") {
       init_eps <- rep(0, ncol(HA))
-      opt <- optim(par = init_eps, risk_eps, HA = HA, tmleenv = tmleenv, method = "L-BFGS-B")
+      opt <- optim(par = init_eps, risk_eps, HA = HA, tmleenv = tmleenv, method = "L-BFGS-B", submodel=submodel, loss=loss)
       eps <- opt$par
     }
 
-    risk <- risk_eps(eps, HA, tmleenv)
+    risk <- risk_eps(eps, HA, tmleenv,submodel=submodel,loss=loss)
 
     # check for improvement
-    if (risk >= last_risk) {
+    if ((risk >= last_risk) || all(abs(ED) < ED2/n)) {
       # we failed to improve, so give up
       j <- j - 1
       converge <- T
       break
-    }
-    if (all(abs(ED) < sig/n)) {
+    } else {
       # update Qk
       tmleenv$Qk <- as.vector(eval(submodel, list(HA = HA, eps = eps, Qk = tmleenv$Qk)))
       tmleenv$Q1k <- as.vector(eval(submodel, list(HA = H1, eps = eps, Qk = tmleenv$Q1k)))
       tmleenv$Q0k <- as.vector(eval(submodel, list(HA = H0, eps = eps, Qk = tmleenv$Q0k)))
-      evals <- lapply(params, eval_param, tmleenv)
-      Dstar <- sapply(evals, `[[`, "IC")
-      ED <- apply(Dstar, 2, mean)
-      psi <- sapply(evals, `[[`, "psi")
-      converge <- T
-      break
-    } else {
-      tmleenv$Qk <- as.vector(eval(submodel, list(HA = HA, eps = eps, Qk = tmleenv$Qk)))
-      tmleenv$Q1k <- as.vector(eval(submodel, list(HA = H1, eps = eps, Qk = tmleenv$Q1k)))
-      tmleenv$Q0k <- as.vector(eval(submodel, list(HA = H0, eps = eps, Qk = tmleenv$Q0k)))
     }
+
 
   }
 
+  ED <- sapply(evals, function(param) mean(param$IC))
   ED2 <- sapply(evals, function(param) mean(param$IC^2))
   ED3 <- sapply(evals, function(param) mean(param$IC^3))
-  result <- list(initdata = initdata, tmledata = data.frame(as.list(tmleenv)), initests = initests,
-                 tmleests = psi, steps = j, Dstar = Dstar, ED = ED, ED2 = ED2, ED3 = ED3)
+  result <- list(initdata = initdata, tmledata = data.frame(as.list(tmleenv)), initests = initests, tmleests = psi, steps = j,
+                 Dstar = Dstar, ED = ED, ED2 = ED2, ED3 = ED3)
 
   class(result) <- "gentmle"
 
@@ -108,28 +100,28 @@ gentmle <- function(initdata, params, submodel = submodel_logit, loss = loss_log
 #' @export
 ci_gentmle <- function(gentmle_obj, level = 0.95) {
 
-    n <- nrow(gentmle_obj$initdata)
-    n_ests <- length(gentmle_obj$tmleests)
-    ldply(seq_len(n_ests), function(i) {
+  n <- nrow(gentmle_obj$initdata)
+  n_ests <- length(gentmle_obj$tmleests)
+  ldply(seq_len(n_ests), function(i) {
 
-        est <- gentmle_obj$tmleests[i]
-        se <- sqrt(gentmle_obj$ED2[i])/sqrt(n)
-        z <- (1 + level)/2
-        lower <- est - qnorm(z) * se
-        upper <- est + qnorm(z) * se
-        data.frame(parameter = names(est), est = est, se = se, lower = lower, upper = upper)
-    })
+    est <- gentmle_obj$tmleests[i]
+    se <- sqrt(gentmle_obj$ED2[i])/sqrt(n)
+    z <- (1 + level)/2
+    lower <- est - qnorm(z) * se
+    upper <- est + qnorm(z) * se
+    data.frame(parameter = names(est), est = est, se = se, lower = lower, upper = upper)
+  })
 }
 
 #' @export
 print.gentmle <- function(gentmle_obj) {
-    cat(sprintf("TMLE ran for %d step(s)\n", gentmle_obj$steps))
-    EDtext <- sprintf("E[%s]=%1.2e", names(gentmle_obj$ED), gentmle_obj$ED)
-    cat(sprintf("The mean of the IC is %s\n", paste(EDtext, collapse = ", ")))
+  cat(sprintf("TMLE ran for %d step(s)\n", gentmle_obj$steps))
+  EDtext <- sprintf("E[%s]=%1.2e", names(gentmle_obj$ED), gentmle_obj$ED)
+  cat(sprintf("The mean of the IC is %s\n", paste(EDtext, collapse = ", ")))
 
-    cat("\n\n")
-    print(ci_gentmle(gentmle_obj))
+  cat("\n\n")
+  print(ci_gentmle(gentmle_obj))
 
-    cat("\n")
+  cat("\n")
 
 }
