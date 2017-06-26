c34320fb88b521efe9316adf3f82f7f4decb0100
Jonathan Levy <jl@jlstiles.com>
2017-06-26 14:23:23 -0700

    more refining of examples

--- inst/examples/eysigmaATE_example.R
+++ inst/examples/eysigmaATE_example.R
@@ -9,14 +9,26 @@ print(result)
 
 # for iterative TMLE, choose full--different approaches sometimes give slightly
 # different answers
+
+# full approach computes a separate epsilon for each parameter
 result <- gentmle(initdata = initdata, params = list(param_ATE, param_sigmaATE),
                   approach = "full")
 print(result)
 
+# line approach is the iterative analog of the 1 step TMLE
+result <- gentmle(initdata = initdata, params = list(param_ATE, param_sigmaATE),
+                  approach = "line")
+print(result)
+
+# recursive is the 1 step tmle as in Mark van der Laan's recent work
+result <- gentmle(initdata = initdata, params = list(param_ATE, param_sigmaATE),
+                  approach = "recursive", max_iter = 10000)
+
 # One can also form simultaneous confidence bounds for numerous params using the
 # influence curves by specifying simultaneous.inference = TRUE
+
 result <- gentmle(initdata = initdata, params = list(param_ATE, param_sigmaATE),
-                  approach = "recursive", simultaneous.inference = TRUE)
+                  approach = "recursive", max_iter = 10000, simultaneous.inference = TRUE)
 print(result)
 
 
