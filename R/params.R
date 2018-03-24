2dd4e64929040e6a48b99284d2badcaccfa5fb70
Jonathan Levy <jl@jlstiles.com>
2018-03-24 11:33:01 -0700

    need a mean of blip squared param so as to target mean of blip sqared and mean of blip, then do delta method to get the blip variance--reason--cv-tmle of blip variance I believe is not right because updates for bar{Q}_{n,B_n} then depend on Q_{W,n,B_n} updates and we do not want to keep track of those.  For mean of blip squared and mean of blip this problem does not occur so we never have to do updates of Q_W because it always gets updated to the empirical on the val fold.

--- R/params.R
+++ R/params.R
@@ -9,5 +9,13 @@ param_ATE <- define_param(psi = "mean(Q1k-Q0k)", HA = "(A/gk - (1 - A)/(1 - gk))
 
 # V_W[E[Y|A=1,W]-E[Y|A=0,W]]
 #' @export
-param_sigmaATE <- define_param(psi = "mean((Q1k-Q0k-mean(Q1k-Q0k))^2)", HA = "2 * (Q1k - Q0k - mean(Q1k-Q0k)) * (A/gk - (1 - A)/(1 - gk))", 
-    IC = "HA*(Y-Qk)+(Q1k - Q0k - mean(Q1k-Q0k))^2 - psi") 
+param_sigmaATE <- define_param(psi = "mean((Q1k-Q0k-mean(Q1k-Q0k))^2)", HA = "2 * (Q1k - Q0k - mean(Q1k-Q0k)) * (A/gk - (1 - A)/(1 - gk))",
+    IC = "HA*(Y-Qk)+(Q1k - Q0k - mean(Q1k-Q0k))^2 - psi")
+
+#' @export
+param_ATT <- define_param(psi = "mean((Q1k-Q0k)*gk/mean(A))", HA = "A/mean(A) - (1-A) * gk / (mean(A) * (1-gk))",
+                          CA = "(Q1k - Q0k - psi)/mean(A)", IC = "HA*(Y-Qk) + CA*(A - gk) + (Q1k - Q0k - psi)*gk/mean(A)")
+
+#' @export
+param_EB2 <- define_param(psi = "mean((Q1k-Q0k)^2)", HA = "2 * (Q1k - Q0k) * (A/gk - (1 - A)/(1 - gk))",
+                          IC = "HA*(Y-Qk)+(Q1k - Q0k)^2 - psi")
